d98b4730a1b5a59642a09477bd118fe7ab7de77f
Afif Al Mamun <afifanik@gmail.com>
2021-01-23 12:23:04 +0600

    User auth

--- template_project/learning_templates/basic_app/views.py
+++ template_project/learning_templates/basic_app/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'basic_app/index.html')
+    context = {
+        'name': "Afif",
+        "number": 7979
+    }
+    return render(request, 'basic_app/index.html', context)
     
 def relative(request):
     return render(request, 'basic_app/relative_url_templates.html')
